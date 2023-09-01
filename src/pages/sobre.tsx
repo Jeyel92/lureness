ed64cebfa08b0d73fa5dd168d19e6767d0e15667
Jeiel Benedito <jeiel.benedito@gmail.com>
2023-09-01 18:02:07 -0300

    feat: add sessão estatisticas, página Sobre

--- src/pages/sobre.tsx
+++ src/pages/sobre.tsx
@@ -14,6 +14,30 @@ export default function SobrePage() {
           <span className=" font-normal">de forma única no mercado.</span>
         </h1>
       </Hero>
+      <section>
+        <div className="container px-5 py-10  mx-auto">
+          <div className="flex flex-col text-left w-full mb-5">
+            <p className=" text-zinc-900 text-base font-normal leading-tight">
+              Branding é Plataforma de Lucro - Marcas Fortes Vendem Mais e
+              Melhor.
+            </p>
+          </div>
+          <div className="flex flex-row justify-center text-zinc-900 text-center">
+            <div className="p-4 ">
+              <h2 className="font-bold text-3xl">+ 700</h2>
+              <p className="text-lg font-normal">clientes</p>
+            </div>
+            <div className="p-4">
+              <h2 className="font-bold   text-3xl ">+ 700</h2>
+              <p className="text-lg font-normal">ROI</p>
+            </div>
+            <div className="p-4">
+              <h2 className="font-bold text-3xl ">+ 700</h2>
+              <p className="text-lg font-normal">faturamento</p>
+            </div>
+          </div>
+        </div>
+      </section>
     </Layout>
   )
 }
