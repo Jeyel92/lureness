e28eb3e718f81eedec84e8603ce7d92cb7cb1005
Jeiel Benedito <jeiel.benedito@gmail.com>
2023-09-02 15:17:32 -0300

    feat: add background image da página

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,10 +4,10 @@ import Footer from '~/components/Footer'
 import Header from '~/components/Header'
 export default function Layout({ children }) {
   return (
-    <>
+    <div className="bg-[url('/bgPage.svg')] bg-repeat-y bg-cover">
       <Header />
       {children}
       <Footer />
-    </>
+    </div>
   )
 }
