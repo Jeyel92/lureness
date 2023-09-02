0e00ab70d5f46becba73dafd47ef6a2148e9ae50
Jeiel Benedito <jeiel.benedito@gmail.com>
2023-09-01 22:04:28 -0300

    feat: inclusão fonts layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '~/styles/global.css'
 
-import { IBM_Plex_Mono, Inter, PT_Serif } from '@next/font/google'
+import { Epilogue, Libre_Baskerville } from '@next/font/google'
 import type { AppProps } from 'next/app'
 import { lazy } from 'react'
 
@@ -11,19 +11,13 @@ export interface SharedPageProps {
 
 const PreviewProvider = lazy(() => import('~/components/PreviewProvider'))
 
-const mono = IBM_Plex_Mono({
-  variable: '--font-family-mono',
-  subsets: ['latin'],
-  weight: ['500', '700'],
-})
-
-const sans = Inter({
+const sans = Epilogue({
   variable: '--font-family-sans',
   subsets: ['latin'],
   weight: ['500', '700', '800'],
 })
 
-const serif = PT_Serif({
+const serif = Libre_Baskerville({
   variable: '--font-family-serif',
   style: ['normal', 'italic'],
   subsets: ['latin'],
@@ -42,7 +36,6 @@ export default function App({
           :root {
             --font-family-sans: ${sans.style.fontFamily};
             --font-family-serif: ${serif.style.fontFamily};
-            --font-family-mono: ${mono.style.fontFamily};
           }
         `}
       </style>
