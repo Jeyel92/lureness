fd4878a6cd15cb45e17c02675318bc6dabaa49bc
Jeiel Benedito <jeiel.benedito@gmail.com>
2023-09-03 20:28:11 -0300

    feat: add cta página sobre

--- src/pages/sobre.tsx
+++ src/pages/sobre.tsx
@@ -1,4 +1,5 @@
 import Accordion from '~/components/Accordion'
+import Cta from '~/components/Cta'
 import Hero from '~/components/Hero'
 import Layout from '~/components/Layout'
 
@@ -26,6 +27,14 @@ export default function SobrePage() {
         </div>
       </section>
       <Accordion />
+      <Cta bgColor="bg-zinc-900" customClass="gap-2">
+        <p className="w-[85%] md:w-3/5 text-white text-2xl font-medium md:text-5xl md:font-semibold">
+          Nós temos a solução ideal para a sua empresa!
+        </p>
+        <div className=" text-slate-50 text-base font-medium underline md:text-white md:text-3xl md:font-bold mt-2">
+          Fale com a Lureness
+        </div>
+      </Cta>
     </Layout>
   )
 }
