d5420b599ac368c0fa544ca51af92410db35ff9b
Jeiel Benedito <jeiel.benedito@gmail.com>
2023-09-08 23:47:25 -0300

    fix: retirado fixed header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,7 +19,7 @@ export default function Header() {
     <>
       <header>
         <nav
-          className="flex px-5 py-3 md:py-0 justify-between items-center bg-slate-50 shadow w-full fixed"
+          className="flex px-5 py-3 md:py-0 justify-between items-center bg-slate-50 shadow"
           aria-label="Global"
         >
           <div className="w-24 h-6 justify-center items-center inline-flex">
