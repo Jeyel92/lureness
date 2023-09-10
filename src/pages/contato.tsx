b4bfc038dc559f362d9b4ca8b4bcb98e643608d7
Jeiel Benedito <jeiel.benedito@gmail.com>
2023-09-10 15:05:35 -0300

    feat: teste envio email

--- src/pages/contato.tsx
+++ src/pages/contato.tsx
@@ -27,6 +27,7 @@ export default function ContatoPage() {
         </div>
         <form
           data-static-form-name="contact"
+          method="post"
           className="mx-auto mt-16 max-w-xl sm:mt-20"
         >
           <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
