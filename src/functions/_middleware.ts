7db250cccea8c38558dff4d3b028ca8be230356b
Jeiel Benedito <jeiel.benedito@gmail.com>
2023-09-10 13:21:30 -0300

    feat: add configuração Worker para envio de email

---
+++ src/functions/_middleware.ts
@@ -0,0 +1,18 @@
+import mailChannelsPlugin from '@cloudflare/pages-plugin-mailchannels'
+
+export const onRequest = mailChannelsPlugin({
+  personalizations: [
+    {
+      to: [{ name: 'ACME Support', email: 'jeiel.benedito@gmail.com' }],
+    },
+  ],
+  from: {
+    name: 'ACME Support',
+    email: 'support@example.com',
+  },
+  respondWith: () => {
+    return new Response(
+      `Thank you for submitting your enquiry. A member of the team will be in touch shortly.`,
+    )
+  },
+})
--- src/pages/contato.tsx
+++ src/pages/contato.tsx
@@ -26,8 +26,7 @@ export default function ContatoPage() {
           </h2>
         </div>
         <form
-          action="#"
-          method="POST"
+          data-static-form-name="contact"
           className="mx-auto mt-16 max-w-xl sm:mt-20"
         >
           <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
